cfbe6565e278acb35b7480a910ddab7e6d580205
EugeniaOstrikova <genechka.ostrikova@gmail.com>
2018-02-26 16:41:44 +0300

    loveTriangles

--- src/index.js
+++ src/index.js
@@ -4,4 +4,21 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+    var count = 0;
+    for(var i=0; i<preferences.length; i++){
+        var spichoneeA = i+1;
+        var spichoneeB = preferences[i];
+        var spichoneeC = preferences[spichoneeB - 1];
+        if(spichoneeB<spichoneeA||spichoneeC<spichoneeA)continue;
+        else if(spichoneeA==spichoneeB==spichoneeC)continue;
+        else if(preferences[spichoneeC - 1] == spichoneeA){
+            count = count +1;
+        }
+        return count;
+        console.log("i=" + i);
+        console.log("spichoneeA=" + spichoneeA);
+        console.log("spichoneeB=" + spichoneeB);
+        console.log("spichoneeC=" + spichoneeC);
+        console.log("count=" + count);
+    }
 };
